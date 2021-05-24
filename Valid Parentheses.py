1425e386d6a17bbb1e999a92080a0dec0d7708ac
Manish <81637340+222010303047@users.noreply.github.com>
2021-05-24 11:23:37 +0530

    Valid Parentheses program added.

---
+++ Valid Parentheses.py
@@ -0,0 +1,12 @@
+def solve(s):
+    count = 0
+    for i in s:
+        if i == "(" or i == "[" or i == "{":
+            count+=1
+        if i == ")" or i == "]" or i == "}":
+            count-=1
+            if count < 0:
+                return False
+    return count == 0
+
+print(solve("(){}{}[])("))
